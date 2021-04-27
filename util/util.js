f9ac07e32cc0f91578b813019318c9632fc7294f
zhangli <zhangli@flashhold.com>
2021-04-27 19:03:02 +0800

    moving-sprites

---
+++ util/util.js
@@ -0,0 +1,12 @@
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Rectangle = PIXI.Rectangle;
+
+const _urls = [
+    "https://raw.githubusercontent.com/zlzbt/Pixi_myFirst/master/images/Doraemon1.jpg",
+    "https://raw.githubusercontent.com/zlzbt/Pixi_myFirst/master/images/cat1.png",
+];
